e3dd8f73260cae889a2e20eebdf624f1d82a8ec0
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-06-02 23:29:19 +0000

    Add text input fields on Add record page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      maxDate: new Date(),
+      yearRange: 91,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
+    });
   });
