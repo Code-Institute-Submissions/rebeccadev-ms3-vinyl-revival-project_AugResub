0df126ddaa85b8901d33eb899ea508ec8cc30620
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-07-24 17:31:29 +0000

    Testing Html Validator for duplicate id

--- static/js/add_remove_field.js
+++ static/js/add_remove_field.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var maxField = 15; //Input fields increment limitation
     var addButton = $('.add_button'); //Add button selector
     var wrapper = $('.field_wrapper'); //Input field wrapper
-    var fieldHTML = '<div><input type="text" id="tracklisting" name="tracklisting"/><a href="javascript:void(0);" class="remove_button right"><img src="static/images/remove-button.png"/></a></div>'; //New input field html 
+    var fieldHTML = '<div><input type="text" name="tracklisting"/><a href="javascript:void(0);" class="remove_button right"><img src="static/images/remove-button.png"/></a></div>'; //New input field html 
     var x = 1; //Initial field counter is 1
     
     //Once add button is clicked
